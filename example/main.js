ccabd48b3ece76f0ed38a41c930e5a4843a032e3
Madimetja Shika <mmjshika@gmail.com>
2019-12-31 14:36:23 +0200

    Cleaned up example project...

--- example/main.js
+++ example/main.js
@@ -3,12 +3,11 @@
 import Vue from 'vue';
 import VueClipboards from 'vue-clipboards';
 import Vuetify from 'vuetify';
-// import Vuetify from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css';
 import App from './App';
 import router from './router';
-// import VuetifyGoogleAutocomplete from '../src/vga/index';
-import VuetifyGoogleAutocomplete from '../lib/index';
+import VuetifyGoogleAutocomplete from '../src/vga/index';
+// import VuetifyGoogleAutocomplete from '../lib/index';
 
 Vue.use(VueClipboards);
 
@@ -16,8 +15,7 @@ const vuetifyOptions = {};
 Vue.use(Vuetify);
 
 Vue.use(VuetifyGoogleAutocomplete, {
-  // apiKey: 'AIzaSyDyJUwEpWPLTDZrX9TVeq5m8vGQScqyZCA',
-  apiKey: 'AIzaSyCKizLJY8yg_bPu1rPUzo8-c9JZ5jXDeU0',
+  apiKey: 'AIzaSyDyJUwEpWPLTDZrX9TVeq5m8vGQScqyZCA',
 });
 
 Vue.config.productionTip = false;
